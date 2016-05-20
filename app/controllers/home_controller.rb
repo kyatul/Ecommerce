daeb37e275cbe463f8b2a034ae2360f12beeca99
kyatul <atul.21j@gmail.com>
2016-05-20 14:27:07 +0530

    Show products at home page, category wise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
 
   def index
+    @categories = Category.all
+  end
+
+  def category_products
+    @products = Category.find(params[:id]).products
   end
 end
